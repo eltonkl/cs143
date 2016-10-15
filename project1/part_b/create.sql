d550b66d0d223f4904af550a81921492696ad810
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-10-15 16:02:27 -0700

    Use InnoDB; add DROP helper script

--- project1/part_b/create.sql
+++ project1/part_b/create.sql
@@ -4,7 +4,7 @@ CREATE TABLE Movie(
     year INT,
     rating VARCHAR(10),
     company VARCHAR(50)
-);
+) ENGINE = INNODB;
 
 CREATE TABLE Actor(
     id INT,
@@ -13,7 +13,7 @@ CREATE TABLE Actor(
     sex VARCHAR(6),
     dob DATE,
     dod DATE
-);
+) ENGINE = INNODB;
 
 CREATE TABLE Director(
     id INT,
@@ -21,23 +21,23 @@ CREATE TABLE Director(
     first VARCHAR(20),
     dob DATE,
     dod DATE
-);
+) ENGINE = INNODB;
 
 CREATE TABLE MovieGenre(
     mid INT,
     genre VARCHAR(20)
-);
+) ENGINE = INNODB;
 
 CREATE TABLE MovieDirector(
     mid INT,
     did INT
-);
+) ENGINE = INNODB;
 
 CREATE TABLE MovieActor(
     mid INT,
     aid INT,
     role VARCHAR(50)
-);
+) ENGINE = INNODB;
 
 CREATE TABLE Review(
     name VARCHAR(20),
@@ -45,12 +45,12 @@ CREATE TABLE Review(
     mid INT,
     rating INT,
     comment VARCHAR(500)
-);
+) ENGINE = INNODB;
 
 CREATE TABLE MaxPersonID(
     id INT
-);
+) ENGINE = INNODB;
 
 CREATE TABLE MaxMovieID(
     id INT
-);
+) ENGINE = INNODB;
