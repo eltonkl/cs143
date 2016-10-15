781a945b114d40421bcb73e31e0d33e97da7c9d2
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-10-15 16:26:48 -0700

    Add integrity constraints to create.sql

--- project1/part_b/create.sql
+++ project1/part_b/create.sql
@@ -3,7 +3,8 @@ CREATE TABLE Movie(
     title VARCHAR(100),
     year INT,
     rating VARCHAR(10),
-    company VARCHAR(50)
+    company VARCHAR(50),
+    PRIMARY KEY(id)
 ) ENGINE = INNODB;
 
 CREATE TABLE Actor(
@@ -12,7 +13,9 @@ CREATE TABLE Actor(
     first VARCHAR(20),
     sex VARCHAR(6),
     dob DATE,
-    dod DATE
+    dod DATE,
+    PRIMARY KEY(id),
+    CHECK (dob <= CURRENT_DATE())
 ) ENGINE = INNODB;
 
 CREATE TABLE Director(
@@ -20,23 +23,30 @@ CREATE TABLE Director(
     last VARCHAR(20),
     first VARCHAR(20),
     dob DATE,
-    dod DATE
+    dod DATE,
+    PRIMARY KEY(id),
+    CHECK (dob <= dod OR dob IS NULL OR dod IS NULL)
 ) ENGINE = INNODB;
 
 CREATE TABLE MovieGenre(
     mid INT,
-    genre VARCHAR(20)
+    genre VARCHAR(20),
+    FOREIGN KEY(mid) REFERENCES Movie(id)
 ) ENGINE = INNODB;
 
 CREATE TABLE MovieDirector(
     mid INT,
-    did INT
+    did INT,
+    FOREIGN KEY(mid) REFERENCES Movie(id),
+    FOREIGN KEY(did) REFERENCES Director(id)
 ) ENGINE = INNODB;
 
 CREATE TABLE MovieActor(
     mid INT,
     aid INT,
-    role VARCHAR(50)
+    role VARCHAR(50),
+    FOREIGN KEY(mid) REFERENCES Movie(id),
+    FOREIGN KEY(aid) REFERENCES Actor(id)
 ) ENGINE = INNODB;
 
 CREATE TABLE Review(
@@ -44,7 +54,9 @@ CREATE TABLE Review(
     time TIMESTAMP,
     mid INT,
     rating INT,
-    comment VARCHAR(500)
+    comment VARCHAR(500),
+    FOREIGN KEY(mid) REFERENCES Movie(id),
+    CHECK (0 <= rating AND rating <= 5)
 ) ENGINE = INNODB;
 
 CREATE TABLE MaxPersonID(
