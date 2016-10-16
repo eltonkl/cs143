f4261efdf25c702108c35874b8752bf9ad02ad4a
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-10-15 22:23:54 -0700

    Add constraints comment

--- project1/part_b/create.sql
+++ project1/part_b/create.sql
@@ -4,7 +4,7 @@ CREATE TABLE Movie(
     year INT,
     rating VARCHAR(10),
     company VARCHAR(50),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id)     -- Movie id should be a primary key as it uniquely identifies each Movie
 ) ENGINE = INNODB;
 
 CREATE TABLE Actor(
@@ -14,8 +14,8 @@ CREATE TABLE Actor(
     sex VARCHAR(6),
     dob DATE,
     dod DATE,
-    PRIMARY KEY(id),
-    CHECK (dob <= CURRENT_DATE())
+    PRIMARY KEY(id),    -- Actor id should be a primary key as it uniquely identifies each Actor
+    CHECK (dob <= CURRENT_DATE())   -- date of birth should not be in the future
 ) ENGINE = INNODB;
 
 CREATE TABLE Director(
@@ -24,29 +24,29 @@ CREATE TABLE Director(
     first VARCHAR(20),
     dob DATE,
     dod DATE,
-    PRIMARY KEY(id),
-    CHECK (dob <= dod OR dob IS NULL OR dod IS NULL)
+    PRIMARY KEY(id),    -- Director id should be a primary key as it uniquely identifies each Director
+    CHECK (dob <= dod OR dob IS NULL OR dod IS NULL)    -- If dod and dob is NOT NULL, dob should not be after dod
 ) ENGINE = INNODB;
 
 CREATE TABLE MovieGenre(
     mid INT,
     genre VARCHAR(20),
-    FOREIGN KEY(mid) REFERENCES Movie(id)
+    FOREIGN KEY(mid) REFERENCES Movie(id)   -- mid should be a foreign key referencing Movie id
 ) ENGINE = INNODB;
 
 CREATE TABLE MovieDirector(
     mid INT,
     did INT,
-    FOREIGN KEY(mid) REFERENCES Movie(id),
-    FOREIGN KEY(did) REFERENCES Director(id)
+    FOREIGN KEY(mid) REFERENCES Movie(id),  -- mid should be a foreign key referencing Movie id
+    FOREIGN KEY(did) REFERENCES Director(id)-- did should be a foreign key referencing Director id
 ) ENGINE = INNODB;
 
 CREATE TABLE MovieActor(
     mid INT,
     aid INT,
     role VARCHAR(50),
-    FOREIGN KEY(mid) REFERENCES Movie(id),
-    FOREIGN KEY(aid) REFERENCES Actor(id)
+    FOREIGN KEY(mid) REFERENCES Movie(id),  -- mid should be a foreign key referencing Movie id
+    FOREIGN KEY(aid) REFERENCES Actor(id)   -- aid should be a foreign key referencing Director id
 ) ENGINE = INNODB;
 
 CREATE TABLE Review(
@@ -55,8 +55,8 @@ CREATE TABLE Review(
     mid INT,
     rating INT,
     comment VARCHAR(500),
-    FOREIGN KEY(mid) REFERENCES Movie(id),
-    CHECK (0 <= rating AND rating <= 5)
+    FOREIGN KEY(mid) REFERENCES Movie(id),  -- mid should be a foreign key referencing Movie id
+    CHECK (0 <= rating AND rating <= 5)     -- rating should be between 0 and 5, inclusive
 ) ENGINE = INNODB;
 
 CREATE TABLE MaxPersonID(
