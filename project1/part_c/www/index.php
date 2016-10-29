8de7f7a1642f7f2dd7123e2005769f7bb81eceb2
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-10-29 15:19:15 -0700

    Add in index and header pages, add bootstrap

---
+++ project1/part_c/www/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <title>CS143 Project 1C</title>
+
+        <!-- CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <?php
+            // header navigation bar
+            include_once('header.php');
+        ?>
+
+        <div class="container main">
+            <p>hello</p>
+
+        </div>
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
+
+
